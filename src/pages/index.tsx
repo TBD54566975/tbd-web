e05797151d1c50aa7d5c2cdbfae88fe881a0bbb1
dayhaysoos <ndejesus1227@gmail.com>
2024-05-23 18:42:37 -0400

    set up blog card component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,36 +7,67 @@ import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+type BlogCardProps = {
+  date?: string;
+  author?: string;
+  title?: string;
+  tags?: string[];
+  image?: string | null;
+  description?: string;
+  size?: 'large' | 'small';
+};
+
+function BlogCard({
+  date = 'date',
+  author = 'author',
+  title = 'Title',
+  tags = [],
+  image = require('/static/img/test-image.png').default,
+  description = 'Some description here',
+  size = 'small',
+}: BlogCardProps) {
+  const cardSizeClass = size === 'large' ? 'max-w-[840px]' : 'max-w-[400px]';
+
+  console.log('card', cardSizeClass);
   return (
-    <header className={clsx('hero ', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+    <div
+      className={clsx(
+        'group border-[gray] border-2 border-solid bg-dark-grey hover:bg-tbd-yellow text-[#FFF] hover:text-[black]',
+        cardSizeClass
+      )}
+    >
+      <img className="h-auto max-w-full block" src={image} />
+      <div className="p-4">
+        <div>{date}</div>
+        <div className="text-tbd-yellow group-hover:text-[black]">{`@${author}`}</div>
+        <h2 className="text-4xl tracking-wide">{title}</h2>
+        <div className="flex">
+          {tags.map((t) => (
+            <div className="border-[#FFF] group-hover:border-dark-grey border-2 border-solid p-2 m-1.5 h-8 justify-center flex items-center">
+              <span className="text-[gray] mr-2">#</span>
+              {`${t}`}
+            </div>
+          ))}
         </div>
       </div>
-    </header>
+    </div>
   );
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      description="Description will go into a meta tag in <head />"
+    >
+      <header className=" flex justify-center items-center">
+        <h1>TBD Components</h1>
+      </header>
+
       <main>
-        <HomepageFeatures />
+        <BlogCard tags={['Test1', 'Test2', 'Test3']} size="large" />
+        <BlogCard tags={['Test1', 'Test2', 'Test3']} />
       </main>
     </Layout>
   );
