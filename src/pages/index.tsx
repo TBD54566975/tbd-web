46a929ef9674d9d51344492acd345825264490f3
dayhaysoos <ndejesus1227@gmail.com>
2024-05-28 14:08:21 -0400

    oop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,9 +20,9 @@ export default function Home(): JSX.Element {
         <h3>Blog Card - Small</h3>
         <BlogCard tags={['Test1', 'Test2', 'Test3']} />
         <h3>Square Background component (WIP)</h3>
-        {/* <TbdBackground width={50} height={50} primaryColor="#FFEC19" />
+        <TbdBackground width={50} height={50} primaryColor="#FFEC19" />
         <TbdBackground width={50} height={50} primaryColor="#00E5FA" />
-        <TbdBackground width={50} height={50} primaryColor="#9A1AFF" /> */}
+        <TbdBackground width={50} height={50} primaryColor="#9A1AFF" />
       </main>
     </Layout>
   );
